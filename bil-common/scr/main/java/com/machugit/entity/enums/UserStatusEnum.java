26bf9aa3432f03755afba57ca298ef05f3b3eaea
MaChu <2916595675@qq.com>
2026-06-27 13:54:11 +0800

    登录注册

---
+++ bil-common/scr/main/java/com/machugit/entity/enums/UserStatusEnum.java
@@ -0,0 +1,26 @@
+package com.machugit.entity.enums;
+
+public enum UserStatusEnum {
+    ENABLE(1, "正常"),
+    DISABLE(0, "禁用");
+
+    private Integer status;
+    private String desc;
+
+    UserStatusEnum(Integer status, String desc) {
+        this.status = status;
+        this.desc = desc;
+    }
+
+    public static UserStatusEnum getByStatus(Integer status) {
+        for (UserStatusEnum item : UserStatusEnum.values()) {
+            if (item.getStatus().equals(status)){
+                return item;
+            }
+        }
+        return null;
+    }
+    public Integer getStatus() {return status;}
+    public String getDesc() {return desc;}
+}
+
